a6356bdccc63713ce447618f6e54c47fc9be8c28
Murtaza Vasi <Murtaza-Vasi>
2021-05-27 19:32:55 +0530

    initial commit

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllMeetUps = () => {
+	return <div>All Meetups Page</div>;
+};
+
+export default AllMeetUps;
---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewMeetups = () => {
+	return <div>New Meetups Page</div>;
+};
+
+export default NewMeetups;
