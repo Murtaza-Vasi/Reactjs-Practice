1c96978317bf910e2720e5518a51e785d425e89a
Murtaza Vasi <Murtaza-Vasi>
2021-05-27 23:14:59 +0530

    Routing and Components created to display meetups

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetups from './pages/AllMeetups';
+import Favorites from './pages/Favorites';
+import NewMeetups from './pages/NewMeetups';
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<MainNavigation />
+			<Switch>
+				<Route path='/' exact>
+					<AllMeetups />
+				</Route>
+				<Route path='/favorites'>
+					<Favorites />
+				</Route>
+				<Route path='/new-meetup'>
+					<NewMeetups />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
