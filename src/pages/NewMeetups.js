0a45d9f4ebb32805abd8c5de13a0dec3cae9ef43
Murtaza Vasi <Murtaza-Vasi>
2021-05-28 14:45:58 +0530

    submitting the new meetup form stores the data in firebase

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetup/NewMeetupForm';
 
 const NewMeetups = () => {
+	const history = useHistory();
+
+	const addMeetupHandler = (meetupData) => {
+		fetch(
+			'https://meetup-react-49311-default-rtdb.firebaseio.com/meetups.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(meetupData),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		).then(() => history.replace('/'));
+	};
 	return (
 		<section>
 			<h1>Add New Meetup</h1>
-			<NewMeetupForm />
+			<NewMeetupForm onAddMeetup={addMeetupHandler} />
 		</section>
 	);
 };
